c83bd01bb0f2161beb702ac4792f8d91f10fcea5
Victor Assunção <victor.a.schumann@gmail.com.br>
2023-03-09 18:58:51 -0300

    start react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import { Bstrash, BsBookmarkCheck, BsBookmarkCheckFill } from "react-icons/bs";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const API = "local://localhost:5000";
+
+function App(){
+  const [title, settitle] = useState("")
+  const [time, settime] = useState("")
+  const [todos, setTodos] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log("enviou")
+  }
+
+
+  return(
+      <div className="app">
+        <div className="todo-header">
+          <h1>React Todo</h1>
+        </div>
+        <div className="form-todo">
+          <h2>insira sua tarefa</h2>
+          <form onSubmit={handleSubmit}>
+              <input type="submit" value="enviar" />
+          </form>
+        </div>
+        <div className="list-todo">
+          <h2>lista de tarefas</h2>
+          {todos.length === 0 && <p> não há tarefas!</p>}
+        </div>
+      </div>
+
+
   );
+
+
 }
+
 
 export default App;
